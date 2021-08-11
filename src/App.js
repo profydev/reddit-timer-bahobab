43df1ffc04f81bf6667d30c73c5b6c9063f40e77
Konan Houphoue <bahobab@hotmail.com>
2021-08-11 15:46:55 -0500

    first commit for app skeleton tasks (#1)
    * first commit for app skeleton tasks
    
    * completed create-app-skeleton after review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search"><Search /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+function Home() {
+  return (
+    <div>Home Page</div>
+  );
+}
+
+function Search() {
+  return (
+    <div>Search Page</div>
   );
 }
 
