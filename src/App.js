9be8e881ba7ead94701b23418eedb78eb040e3f4
Konan Houphoue <bahobab@hotmail.com>
2021-08-11 13:34:05 -0500

    completed create-app-skeleton after review

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Route, Link, Switch,
+  BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/search">Search</Link></li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/search"><Search /></Route>
-        <Route path="/"><Home /></Route>
-      </Switch>
-      <div />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search"><Search /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,76 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  /* Montserrat Regular */
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff2") format("woff2"), url("/fonts/Montserrat-Regular.woff") format("woff");
+  }
+
+  /* Montserrat Medium */
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff2") format("woff2"), url("/fonts/Montserrat-Medium.woff") format("woff");
+  }
+  
+  /* Montserrat Italic */
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: italic;
+    src: url("/fonts/Montserrat-Italic.woff2") format("woff2"), url("/fonts/Montserrat-Italic.woff") format("woff");
+  }
+
+  /* Montserrat SemiBold */
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff2") format("woff2"), url("/fonts/Montserrat-SemiBold.woff") format("woff");
+  }
+
+  /* Bitter Bold */
+  @font-face {
+    font-family: "Bitter";
+    font-weight: 700;
+    font-style: bold;
+    src: url("/fonts/Bitter-Bold.woff2") format("woff2"), url("/fonts/Bitter-Bold.woff") format("woff");
+  }
+
+  /* Bitter Regular */
+  @font-face {
+    font-family: "Bitter";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Bitter-Regular.woff2") format("woff2"), url("/fonts/Bitter-Regular.woff") format("woff");
+  }
+  
+  /* Bitter Italic */
+  @font-face {
+    font-family: "Bitter";
+    font-weight: 400;
+    font-style: italic;
+    src: url("/fonts/Bitter-Italic.woff2") format("woff2"), url("/fonts/Bitter-Italic.woff") format("woff");
+  }
+
+  /* Bitter Bold */
+  @font-face {
+    font-family: "Bitter";
+    font-weight: 700;
+    font-style: bold;
+    src: url("/fonts/Bitter-Bold.woff2") format("woff2"), url("/fonts/Bitter-Bold.woff") format("woff");
+  }
+
+  body {
+    font-family: ${(props) => props.theme.font.family.default};
+    font-size: ${(props) => props.theme.font.size.default};
+    line-height: ${(props) => props.theme.lineHeight.default};
+    color: ${(props) => props.theme.color.text};
+  }
+`;
+
+export default GlobalStyle;
