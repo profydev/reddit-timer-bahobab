7ca3cb072bf51d263e8355a827d5b716b61baa67
Konan Houphoue <bahobab@hotmail.com>
2021-09-25 16:09:56 -0500

    info-section: first draft

---
+++ src/page-home/InfoSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import * as S from './InfoSection.style';
+
+const InfoSction = () => (
+  <section>
+    <S.SectionWrapper>
+      <S.SectionHeader id="how-it-works">How It Works</S.SectionHeader>
+      <p>
+        {/* eslint-disable-next-line max-len */}
+        This app was created during a course on profy.dev with the goal to implement a pixel-perfect real-world application with professionalworkflows and tools like Kanban, ClickUp, Figma, GitHub, pull requests and code reviews. Click here for more information.
+      </p>
+    </S.SectionWrapper>
+    <S.SectionWrapper>
+      <S.SectionHeader id="about">About</S.SectionHeader>
+      <S.AboutList>
+        <S.ListItem>We find the 500 top posts from the past year for a subreddit.</S.ListItem>
+        {/* eslint-disable-next-line max-len */}
+        <S.ListItem>The data is visualized in a heatmap grouped by weekday and hour of the day.</S.ListItem>
+        <S.ListItem>See immediately when to submit your reddit post.</S.ListItem>
+      </S.AboutList>
+    </S.SectionWrapper>
+  </section>
+);
+
+export default InfoSction;
